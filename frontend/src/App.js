665320b49e626e043a2c7d515274d6a759943455
Gonzalo Diaz <gonzalo.diazj@sansano.usm.cl>
2024-10-21 16:22:27 -0300

    header con nombre al recargar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
@@ -6,25 +7,28 @@ import ProductsPage from './components/ProductsPage';
 import ProductDetail from './components/ProductDetail';
 import CartPage from './components/CartPage';
 import AuthPage from './components/AuthPage';
-import AdvancedSearch from './components/AdvancedSearch'; // Importa la búsqueda avanzada
+import AdvancedSearch from './components/AdvancedSearch';
 import Footer from './components/Footer';
+import { UserProvider } from './context/UserContext';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <main style={{ paddingBottom: '50px' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/productos" element={<ProductsPage />} />
-          <Route path="/producto/:id" element={<ProductDetail />} />
-          <Route path="/login" element={<AuthPage />} />
-          <Route path="/carrito" element={<CartPage />} />
-          <Route path="/busqueda-avanzada" element={<AdvancedSearch />} /> {/* Ruta de búsqueda avanzada */}
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+    <UserProvider>
+      <Router>
+        <Header />
+        <main style={{ paddingBottom: '50px' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/productos" element={<ProductsPage />} />
+            <Route path="/producto/:id" element={<ProductDetail />} />
+            <Route path="/login" element={<AuthPage />} />
+            <Route path="/carrito" element={<CartPage />} />
+            <Route path="/busqueda-avanzada" element={<AdvancedSearch />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </UserProvider>
   );
 }
 
