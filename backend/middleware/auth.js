cd0152d1ce7362cde3444dacb260dac11e9d5001
vicentegaetee <vicente.gaeteo@usm.cl>
2024-10-22 16:10:36 -0300

    update middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,13 +3,8 @@ const jwt = require('jsonwebtoken');
 // Middleware para verificar el token JWT
 const authenticateToken = (req, res, next) => {
   // Obtener el token del encabezado Authorization
-  const authHeader = req.header('Authorization');
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Acceso denegado, formato de token incorrecto o no proporcionado' });
-  }
-
-  const token = authHeader.split(' ')[1];
-
+  const token = req.header('Authorization')?.split(' ')[1];
+  
   if (!token) {
     return res.status(401).json({ message: 'Acceso denegado, token no proporcionado' });
   }
@@ -20,8 +15,7 @@ const authenticateToken = (req, res, next) => {
     req.user = verified;  // Guardar el usuario verificado en req.user para usarlo en las rutas
     next();  // Continuar al siguiente middleware o controlador
   } catch (error) {
-    console.error('Error verificando el token:', error.message); // Log del error para facilitar depuración
-    return res.status(403).json({ message: 'Token no válido o expirado' });
+    res.status(403).json({ message: 'Token no válido' });
   }
 };
 
