e76fcb698257868d146595f972735ce619cad7aa
CharlesLakes <carloslagoscortes@gmail.com>
2024-11-12 23:03:48 -0300

    delete unused reports files

--- backend/routes/reports.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getReport, createReport } = require('../controllers/reports');
-
-const router = express.Router();
-
-router.get('/get-report', getReport);
-router.post('/add-report', createReport);
-
-module.exports = router;
